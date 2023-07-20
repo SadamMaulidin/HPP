16de3547e85ea562e1f07c81e2555cc2d8f44e33
dama <sadammaulidin@gmail.com>
2023-07-20 19:18:18 +0700

    add splash screen, bussiness profile 0.1

---
+++ app/src/main/java/com/example/hpp/model/BussinessModel.kt
@@ -0,0 +1,12 @@
+package com.example.hpp.model
+
+data class BussinessModel(
+    var bussiness_name: String? = null,
+    var bussiness_type: String? = null,
+    var address: String? = null,
+    var phone_num: String? = null,
+    var owner: String? = null,
+    var email: String? = null,
+    var password: String? = null,
+
+)
